4faecb5145adf7d0f7e88dd9539111a6d6931466
PJ Rape <ptrj96@gmail.com>
2020-04-15 01:58:02 -0600

    finish up structure of program

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ CFLAGS = -g -Wall
 
 all: ping
 
-ping.o: ping.C
-	$(CC) $(CFLAGS) -c ping.C
+ping.o: ping.c
+	$(CC) $(CFLAGS) -c ping.c
 
 ping: ping.o
 	$(CC) $(CFLAGS) ping.o -o ping
--- ping.c
+++ ping.c
@@ -7,6 +7,13 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+#include <time.h>
+
+void sigint_handler(int sig);
+void ping(int sockfd, char* host);
+
+int keep_pinging = 1;
 
 int main(int argc, char**argv)
 {
@@ -17,7 +24,10 @@ int main(int argc, char**argv)
     if (argc < 2)
     {
         printf("Usage: %s <hostname/ip address>\n", argv[0]);
+        exit(1);
     }
+
+    signal(SIGINT, sigint_handler);
 
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC; //ipv4 or ipv6
@@ -28,6 +38,7 @@ int main(int argc, char**argv)
     if (error != 0)
     {
         fprintf(stderr, "Error with getaddrinfo\n");
+        exit(1);
     }
 
 
@@ -49,10 +60,39 @@ int main(int argc, char**argv)
     if (result == NULL)
     {
         fprintf(stderr, "Could not connect to %s\n", argv[1]);
+        exit(1);
     }
 
-
-
+    ping(socketfd, argv[1]);
 
     return 0;
+}
+
+void ping(int sockfd, char* host)
+{
+    printf("PING %s\n", host);
+
+    int num_sent = 0, *num_failed = 0;
+    time_t time_start, time_sent, time_received, time_finished;
+
+    time_start = time(NULL);
+
+    while (keep_pinging)
+    {
+
+    }
+
+    time_finished = time(NULL);
+
+    printf("--- %s ping statistics ---\n", host);
+    // printf("%d packets trasnmitted, %d received, %f%% packet loss, time %dms\n");
+    // printf("rtt min/avg/max/mdev = %f/%f/%f/%f ms\n");
+    
+    close(sockfd);
+
+}
+
+void sigint_handler(int sig)
+{
+    keep_pinging = 0;
 }
